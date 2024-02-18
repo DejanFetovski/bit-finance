a1071aa9673f00ff9a065a5d3de47118cb40de77
DejanFetovski <dejanfetovski1@gmail.com>
2024-02-17 21:31:04 -0800

    Custom tailwindcss config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'CircularStd',
+    },
+    extend: {
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      boxShadow: {
+        glow: '0 0 1rem',
+      },
+      aspectRatio: {
+        '7/5': '7 / 5',
+      },
+    },
   },
   plugins: [],
 }
